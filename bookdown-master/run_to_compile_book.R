8171041807a3684783a904ab802c1891e75f43cd
Andrea Miranda Gonzalez <andreamg@demog.berkeley.edu>
2020-07-17 14:45:50 -0700

    Added Ken's presentation and references

--- bookdown-master/run_to_compile_book.R
+++ bookdown-master/run_to_compile_book.R
@@ -1,5 +1,7 @@
 library(bookdown)
-setwd("/hdir/0/fmenares/Book/bookdown-master/")
+# setwd("/hdir/0/fmenares/Book/bookdown-master/")
+setwd("/hdir/0/andreamg/Year2_2019_2020/Random_demography/random_demography/bookdown-master/")
+
 render_book("index.Rmd","bookdown::gitbook",
             clean = TRUE, envir = parent.frame(),
             clean_envir = !interactive(), output_dir = NULL,
