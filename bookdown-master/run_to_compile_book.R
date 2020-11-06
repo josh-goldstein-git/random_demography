4e55fec939133b2e3da6aa10c2f781a11981291e
Felipe Menares <fmenares@demog.berkeley.edu>
2020-11-05 23:06:59 -0800

    update chap 1 final

--- bookdown-master/run_to_compile_book.R
+++ bookdown-master/run_to_compile_book.R
@@ -1,7 +1,7 @@
 library(bookdown)
 library(data.table)
 setwd("/hdir/0/fmenares/Book/bookdown-master/")
-
+#setwd("/hdir/0/andreamg/Year2_2019_2020/Random_demography/random_demography/bookdown-master/")
 render_book("index.Rmd","bookdown::gitbook",
             clean = TRUE, envir = parent.frame(),
             clean_envir = !interactive(), output_dir = NULL,
