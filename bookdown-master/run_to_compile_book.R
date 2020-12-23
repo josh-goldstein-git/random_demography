396a3af2ba135e73b9d2232c5ae88476370421fd
Andrea Miranda Gonzalez <andrea.mirgon@berkeley.edu>
2020-12-22 22:01:15 -0800

    Last changes before compiling

--- bookdown-master/run_to_compile_book.R
+++ bookdown-master/run_to_compile_book.R
@@ -1,6 +1,6 @@
 library(bookdown)
-setwd("/hdir/0/fmenares/Book/bookdown-master/")
-#setwd("/hdir/0/andreamg/Year2_2019_2020/Random_demography/random_demography/bookdown-master/")
+#setwd("/hdir/0/fmenares/Book/bookdown-master/")
+setwd("/hdir/0/andreamg/Year2_2019_2020/Random_demography/random_demography/bookdown-master/")
 render_book("index.Rmd","bookdown::gitbook",
             clean = TRUE, envir = parent.frame(),
             clean_envir = !interactive(), output_dir = NULL,
